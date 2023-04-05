23e1f284144ad2967a0d0fb06947fa6224fd4d82
Musa Azeem <musa.mazeem@gmail.com>
2023-04-05 00:09:26 -0400

    done

--- main.js
+++ main.js
@@ -1,34 +1,38 @@
 const { Client, Events, GatewayIntentBits } = require('discord.js');
 const sendPromptToGPT = require('./utils/sendPromptToGPT')
+const keepAlive = require("./server")
 require('dotenv').config();
 
 
 // Create a new client instance
-const client = new Client({ intents: [
-	GatewayIntentBits.Guilds,
-	GatewayIntentBits.GuildMessages,
-	GatewayIntentBits.MessageContent,
-	GatewayIntentBits.GuildMembers,
-] });
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildMembers,
+  ]
+});
 
 // When the client is ready, run this code (only once)
 // We use 'c' for the event parameter to keep it separate from the already defined 'client'
 client.once(Events.ClientReady, c => {
-	console.log(`Ready! Logged in as ${c.user.tag}`);
+  console.log(`Ready! Logged in as ${c.user.tag}`);
 });
 
 client.on('messageCreate', async (msg) => {
-	const content = msg.content
-	if (content.startsWith('/gpt')) {
-		prompt = content.substring(5)
-		console.log(`\n\nPrompt ${prompt}`)
+  const content = msg.content
+  if (content.startsWith('/gpt')) {
+    prompt = content.substring(5)
+    console.log(`\n\nPrompt ${prompt}`)
 
-		gptRes = await sendPromptToGPT(prompt)
-		msg.reply(gptRes)
-		console.log(`Response: ${gptRes}`)
-	}
+    gptRes = await sendPromptToGPT(prompt)
+    msg.reply(gptRes)
+    console.log(`Response: ${gptRes}`)
+  }
 });
 
 
 // Log in to Discord with token
+keepAlive()
 client.login(process.env.BOT_TOKEN);
